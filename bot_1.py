62f554e6d60feb71a8e5cc82cf6943ac3ed016ca
Ilyas-Mamytov <ilyas120212@gmail.com>
2024-09-08 18:32:17 +0600

    added connecting and exiting voice channels

--- bot_1.py
+++ bot_1.py
@@ -1,78 +1,78 @@
 import os
 import discord
-from datetime import date,timedelta,datetime
+from discord.ext import commands
+from datetime import date, timedelta, datetime
+from dotenv import load_dotenv
 
-class MyClient(discord.Client):
-    
+class MyClient(commands.Bot):
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
-    async def on_message(self, message):
-        
-        if str(message.author) == 'fugu35':
-            msg = 'figus moment'
-        
-            if message.content.lower().find('инет') !=- 1:
-                filename='light.png'
-                date_file='fuggus_not_unet_day.txt'
-                
-            elif message.content.lower().find('свет') !=- 1:
-                filename='dark.jpg'
-                date_file='fuggus_last_day.txt'
-            else:
-                return
-    
-            if not os.path.exists(date_file):
-                with open(date_file, 'w') as f:
-                    vchera = date.today() - timedelta(days=1)
-                    f.write(vchera.strftime('%d %b %y'))
-        
-            with open(date_file, 'r') as f:
-                last_day = f.read()
-                last_day = datetime.strptime(last_day, '%d %b %y').date()
-            
-            
-            today = date.today()
-            if not last_day == today:
-                print(last_day,today)
-                file = discord.File(filename)
-                embed = discord.Embed()
-                embed.set_image(url=f'attachment://{filename}')
-                last_day = today
-                
-                with open(date_file, 'w') as f:
-                    f.write(last_day.strftime('%d %b %y'))
-                    
-                await message.reply(msg, file=file, embed=embed)
-                
-        if str(message.author) == 'lpodok':
-            msg='плачь, дурень😭'
-            if not os.path.exists('lpodok_last_day.txt'):
-                with open('lpodok_last_day.txt', 'w') as f:
-                    vchera=date.today() - timedelta(days=1)
-                    f.write(vchera.strftime('%d %b %y'))
-                    
-            with open('lpodok_last_day.txt', 'r') as f:
-                last_day = f.read()
-                last_day = datetime.strptime(last_day, '%d %b %y').date()
-                
-            today = date.today()
-            print(today,last_day)
-            print(type(today),type(last_day))
-            if not last_day == today:
-                
-                last_day = today       
-           
-                with open('lpodok_last_day.txt', 'w') as f:
-                    f.write(last_day.strftime('%d %b %y'))
-                        
-                await message.reply(msg)
-            
-intents = discord.Intents.default()
-intents.message_content = True
-
-token=os.getenv('DISCORD_TOKEN')
-
-client = MyClient(intents=intents)
-client.run(token)
-
+#     async def on_message(self, message):
+#
+#         if str(message.author) == 'chelhfj':  #fugu35
+#             msg = 'figus moment'
+#
+#             if message.content.lower().find('инет') != - 1:
+#                 filename = 'light.png'
+#                 date_file = 'fuggus_not_unet_day.txt'
+#
+#             elif message.content.lower().find('свет') != - 1:
+#                 filename = 'dark.jpg'
+#                 date_file = 'fuggus_last_day.txt'
+#             else:
+#                 return
+#
+#             if not os.path.exists(date_file):
+#                 with open(date_file, 'w') as f:
+#                     vchera = date.today() - timedelta(days=1)
+#                     f.write(vchera.strftime('%d %b %y'))
+#
+#             with open(date_file, 'r') as f:
+#                 last_day = f.read()
+#                 last_day = datetime.strptime(last_day, '%d %b %y').date()
+#
+#             today = date.today()
+#             if not last_day == today:
+#                 print(last_day, today)
+#                 file = discord.File(filename)
+#                 embed = discord.Embed()
+#                 embed.set_image(url=f'attachment://{filename}')
+#                 last_day = today
+#
+#                 with open(date_file, 'w') as f:
+#                     f.write(last_day.strftime('%d %b %y'))
+#
+#                 await message.reply(msg, file=file, embed=embed)
+#
+#         if str(message.author) == 'kz213.': #'lpodok':
+#             msg = 'плачь, дурень'
+#             if not os.path.exists('lpodok_last_day.txt'):
+#                 with open('lpodok_last_day.txt', 'w') as f:
+#                     vchera = date.today() - timedelta(days=1)
+#                     f.write(vchera.strftime('%d %b %y'))
+#
+#             with open('lpodok_last_day.txt', 'r') as f:
+#                 last_day = f.read()
+#                 last_day = datetime.strptime(last_day, '%d %b %y').date()
+#
+#             today = date.today()
+#             print(today, last_day)
+#             print(type(today), type(last_day))
+#             if not last_day == today:
+#                 last_day = today
+#
+#                 with open('lpodok_last_day.txt', 'w') as f:
+#                     f.write(last_day.strftime('%d %b %y'))
+#
+#                 await message.reply(msg)
+#
+# if __name__=='__main__':
+#     intents = discord.Intents.default()
+#     intents.message_content = True
+#     load_dotenv()
+#     token = os.getenv('DISCORD_TOKEN')
+#
+#     client = MyClient(intents=intents)
+#     client.run(token)
---
+++ guess_the_melody_bot.py
@@ -0,0 +1,35 @@
+import discord
+from dotenv import load_dotenv
+import os
+from bot_1 import MyClient
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents, command_prefix='!')
+
+
+@client.event
+async def on_ready():
+    print("i'm ready")
+
+
+@client.command()
+async def hello(context):
+    await context.reply('hello')
+
+
+@client.command()
+async def voice(context):
+    if not context.message.author.voice:
+        await context.reply('You are not in a voice channel, Join a voice channel!')
+    else:
+        await context.message.author.voice.channel.connect()
+@client.command()
+async def out_channel(context):
+    await context.voice_client.disconnect()
+
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+client.run(token)
